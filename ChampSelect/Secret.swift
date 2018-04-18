c60581a2c514c2513f55123f2cc10af9e7fdbd68
Chinmay Ghotkar <chinmayg@vt.edu>
2018-04-18 01:04:09 -0700

    Test if gitignore works

--- ChampSelect/Secret.swift
+++ ChampSelect/Secret.swift
@@ -10,6 +10,6 @@ import Foundation
 
 struct Key {
     struct Riot{
-        static let API = "XXXX-XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXX"
+        static let API = "XXXX-XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXXTEST"
     }
 }
